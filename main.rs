72190557f7878bcd9cb00f0c2087bb0c1d8831a0
whiteand <andrewbeletskiy@gmail.com>
2022-08-07 22:55:30 +0300

    added imports to reduce name length

--- main.rs
+++ main.rs
@@ -1,18 +1,21 @@
 mod args;
 mod proto;
 
+use args::get_proto_folder_path;
 use args::CliArguments;
+use proto::compiler::ts::compile;
+use proto::folder::read_proto_folder;
+use proto::package::read_packages;
 use std::io;
 
 fn main() -> io::Result<()> {
     let CliArguments {
         proto_folder_path,
         out_folder_path,
-    } = args::get_proto_folder_path()?;
-    let proto_folder = proto::folder::read_proto_folder(proto_folder_path)?;
-    let packages = proto::package::read_packages(&proto_folder.files)?;
-    let ts_files = proto::compiler::ts::compile(&packages, out_folder_path)?;
-    println!("{:?}", ts_files);
+    } = get_proto_folder_path()?;
+    let proto_folder = read_proto_folder(proto_folder_path)?;
+    let packages = read_packages(&proto_folder.files)?;
+    compile(&packages, out_folder_path)?;
 
     Ok(())
 }
